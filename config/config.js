71d70297b20090f1e06ccd1fad1598ff1be8295a
Scott Traver <scottmtraver@gmail.com>
2016-02-19 16:17:57 -0700

    Initial Commit

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'runontrails'
+    },
+    port: 3000,
+    db: 'development:27017/RunOnTrails-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'runontrails'
+    },
+    port: 3000,
+    db: 'development:27017/RunOnTrails-development'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'runontrails'
+    },
+    port: 3000,
+    db: 'runontrailsmongo:27017/RunOnTrails'
+  }
+};
+
+module.exports = config[env];
