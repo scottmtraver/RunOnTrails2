890f51e77e7b34923612f4f569876f1b24b95a5f
Scott Traver <scottmtraver@gmail.com>
2016-02-20 16:55:25 -0700

    public sponsors page now data driven!

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+$(function() {
+  $("#datepicker").datepicker();
+});
+$(document).ready(function () {
+  $('.date-slash').each(function (i, ele) {
+    var formatted = moment(ele.innerHTML).calendar();
+    ele.innerHTML = formatted;
+  });
+  //set venue
+  var vid = $('#venueID').val();
+  $('.venue-selector').val(vid);
+}); 
+
--- public/js/client.js
+++ public/js/client.js
@@ -4,16 +4,10 @@ $(".owl-carousel").owlCarousel({
   singleItem:true
 });
 
-$(function() {
-  $("#datepicker").datepicker();
-});
 $(document).ready(function () {
   $('.date-slash').each(function (i, ele) {
     var formatted = moment(ele.innerHTML).calendar();
     ele.innerHTML = formatted;
   });
-  //set venue
-  var vid = $('#venueID').val();
-  $('.venue-selector').val(vid);
 }); 
 
