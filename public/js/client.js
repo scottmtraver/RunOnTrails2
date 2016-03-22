519ec77c2148b5a9f3d185e794f2c83eba8dc5e5
Scott Traver <scottmtraver@gmail.com>
2016-03-21 22:47:34 -0600

    Added google tracking of homepage video clicks!

--- public/js/client.js
+++ public/js/client.js
@@ -25,3 +25,29 @@ $('.readmore-opener').click(function () {
   $('.readmore-mobile').addClass('open');
   $(this).hide();
 });
+
+// This code loads the IFrame Player API code asynchronously
+var tag = document.createElement('script');
+tag.src = "http://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// This code is called by the YouTube API to create the player object
+function onYouTubeIframeAPIReady(event) {
+  player = new YT.Player('youTubePlayer', {
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+   // do nothing, no tracking needed
+}
+function onPlayerStateChange(event) {
+    // track when user clicks to Play
+    if (event.data == YT.PlayerState.PLAYING) {
+        _gaq.push(['_trackEvent', 'Videos', 'Play', 'Homepage Video']);
+    }
+}
